c548c07e1465f6589f9b9da36395e02ff6da14c0
florinv88 <florinvisan022@gmail.com>
2022-11-03 15:11:36 +0200

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+
+  const handleClick = () => {
+    axios({
+      method: "GET",
+      url: "/fnk"
+    })
+      .then(res => console.log(res))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Click me to get some JSON!</button>
     </div>
   );
 }
